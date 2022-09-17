2eaf52bbcf0a7a06d825e7a702ac024f69fc1b49
TomStevens7533 <tom.stevens2@student.howest.be>
2022-09-17 19:36:35 +0200

    A - Added scaling

--- Europe-Editor/src/Components/ChunkManager.cpp
+++ Europe-Editor/src/Components/ChunkManager.cpp
@@ -2,17 +2,17 @@
 #include "Europa/GameObject.h"
 #include "ChunkComponent.h"
 
-ChunkManager::ChunkManager(const int xSize, int ySize, int zSize, const int chunkWidthAmount, const int chunkDepthAmount, int scale)
+ChunkManager::ChunkManager(const int xSize, int ySize, int zSize, const int chunkWidthAmount, const int chunkDepthAmount, float scale)
 	: m_ChunkxSize{ xSize }, m_ChunkySize{ ySize }, m_ChunkzSize{ zSize }, m_ChunkWidthAmount{ chunkWidthAmount }, m_ChunkDepthAmount{chunkDepthAmount}
 	, m_IsUpdatingAroundCamera{false}, m_Scale{ scale }
 {
 
 }
 
-ChunkManager::ChunkManager(const int xSize, int ySize, int zSize, Eu::PerspectiveCameraControllerComponent* cam, int scale /*= 1*/)
+ChunkManager::ChunkManager(const int xSize, int ySize, int zSize, Eu::PerspectiveCameraControllerComponent* cam, float scale /*= 1*/)
 	: m_ChunkxSize{ xSize }, m_ChunkySize{ ySize }, m_ChunkzSize{ zSize }, m_Scale{scale}, m_Camera{cam}, m_IsUpdatingAroundCamera{true}
 {
-
+	
 }
 
 ChunkManager::~ChunkManager()
@@ -25,28 +25,34 @@ void ChunkManager::Start()
 	if (m_IsUpdatingAroundCamera == false) {
 		//Create chunks based on width and depth around origin gameobject
 		const glm::ivec2 originPos = glm::ivec2{ glm::round(GetAttachedGameObject()->GetTransform().GetPosition().x), glm::round(GetAttachedGameObject()->GetTransform().GetPosition().z) };
-		const int ChunkTotalWidth = m_ChunkWidthAmount * m_ChunkxSize;
-		const int ChunkTotalDepth = m_ChunkDepthAmount * m_ChunkzSize;
+		const int ChunkTotalWidth = m_ChunkWidthAmount  * glm::floor(m_ChunkxSize * m_Scale);
+		const int ChunkTotalDepth = m_ChunkDepthAmount  * glm::floor(m_ChunkzSize * m_Scale);
 
 
 
-		for (int x = (originPos.x - ChunkTotalWidth); x < (originPos.x + ChunkTotalWidth); x += m_ChunkxSize)
+		for (int x = (originPos.x - ChunkTotalWidth); x < (originPos.x + ChunkTotalWidth); x += glm::floor(m_ChunkxSize * m_Scale))
 		{
-			for (int z = (originPos.y - ChunkTotalDepth); z < (originPos.y + ChunkTotalDepth); z +=m_ChunkzSize)
+			for (int z = (originPos.y - ChunkTotalDepth); z < (originPos.y + ChunkTotalDepth); z += glm::floor(m_ChunkzSize * m_Scale))
 			{
 				//Create chunk
-				EU_CORE_INFO("Creating Chunk at ID: {0},{1}", x, z);
-				std::pair<int, int> id = GetChunkID(glm::ivec2{ x,z });
-				auto chunkComp = std::make_shared<ChunkComponent>(m_ChunkxSize, m_ChunkySize, m_ChunkzSize, shared_from_this());
-				m_ChunkIDMap.insert(std::make_pair(id, chunkComp));
-				auto go = std::make_shared<Eu::GameObject>();
-				go->SetPosition(glm::vec3{ x, 0, z });
-				go->AddComponent<ChunkComponent>(chunkComp);
-				GetAttachedGameObject()->AddChild(go);
+				
+				CreateChunk(glm::ivec2{ x,z });
 			}
 		}
 		
 	}
+}
+void ChunkManager::CreateChunk(glm::ivec2 position)
+{
+	EU_CORE_INFO("Creating Chunk at ID: {0},{1}", position.x , position.y );
+	std::pair<int, int> id = GetChunkID(position);
+	auto chunkComp = std::make_shared<ChunkComponent>(m_ChunkxSize, m_ChunkySize, m_ChunkzSize, shared_from_this(), m_Scale);
+	m_ChunkIDMap.insert(std::make_pair(id, chunkComp));
+	auto go = std::make_shared<Eu::GameObject>();
+	go->SetPosition(glm::vec3{ position.x  , 0, position.y });
+	go->GetTransform().Scale(glm::vec3{ m_Scale, m_Scale, m_Scale });
+	go->AddComponent<ChunkComponent>(chunkComp);
+	GetAttachedGameObject()->AddChild(go);
 }
 
 void ChunkManager::Update()
@@ -88,7 +94,7 @@ uint8_t ChunkManager::GetBlockIDNeighbour(glm::ivec2 position, int x, int y, int
 			return  m_ChunkIDMap[id]->GetBlock(x, y, 0);
 		}
 	}
-	return UINT8_MAX;
+	return 0;
 
 }
 
@@ -108,8 +114,8 @@ void ChunkManager::UpdateNeightbours(glm::ivec2 posiotn)
 std::pair<int,int> ChunkManager::GetChunkID(glm::ivec2 position)
 {
 	std::pair<int, int> id;
-	id.first = glm::floor(position.x / m_ChunkxSize);
-	id.second = glm::floor(position.y / m_ChunkzSize);
+	id.first = glm::floor((position.x) / m_ChunkxSize);
+	id.second = glm::floor((position.y ) / m_ChunkzSize);
 	return id;
 
 }
--- Europe-Editor/src/Components/ChunkManager.h
+++ Europe-Editor/src/Components/ChunkManager.h
@@ -7,12 +7,11 @@ class ChunkComponent;
 namespace Eu {
 	class PerspectiveCameraControllerComponent;
 }
-
 class ChunkManager : public Eu::BaseComponent, public std::enable_shared_from_this<ChunkManager>
 {
 public:
-	ChunkManager(const int xSize, int ySize, int zSize, Eu::PerspectiveCameraControllerComponent* cam, int scale = 1);
-	ChunkManager(const int xSize, int ySize, int zSize, const int chunkWidthAmount, const int chunkDepthAmount, int scale = 1);
+	ChunkManager(const int xSize, int ySize, int zSize, Eu::PerspectiveCameraControllerComponent* cam, float scale = 1);
+	ChunkManager(const int xSize, int ySize, int zSize, const int chunkWidthAmount, const int chunkDepthAmount, float scale = 1);
 	~ChunkManager();
 
 
@@ -23,6 +22,7 @@ public:
 	uint8_t GetBlockIDNeighbour(glm::ivec2 position, int x, int y, int z);
 	void UpdateNeightbours(glm::ivec2 posiotn);
 private:
+	void CreateChunk(glm::ivec2 position);
 	std::pair<int, int> GetChunkID(glm::ivec2 position);
 private:
 	bool m_IsUpdatingAroundCamera{ false };
@@ -32,11 +32,12 @@ private:
 	const int m_ChunkzSize{};
 	const int m_ChunkWidthAmount{};
 	const int m_ChunkDepthAmount{};
-	const int m_Scale{};
+	const float m_Scale{};
 	Eu::PerspectiveCameraControllerComponent* m_Camera{};
 	std::map < std::pair<int, int>, std::shared_ptr<ChunkComponent>> m_ChunkIDMap;
 
 
 
 };
+
 
